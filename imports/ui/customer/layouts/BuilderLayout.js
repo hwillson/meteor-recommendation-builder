9ed6b9fbac6fe98020ec091805120d45005da88c
hwillson <hugh@octonary.com>
2016-05-04 14:31:04 -0400

    Working on builder layout.

--- imports/ui/customer/layouts/BuilderLayout.js
+++ imports/ui/customer/layouts/BuilderLayout.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import { Navbar, Grid, Row, Nav, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const BuilderLayout = (props) => (
   <div className="builder">
-    {props.children}
+    <Grid>
+      <Row>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Some Company</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <LinkContainer to={{ pathname: '/builder/welcome' }}>
+              <NavItem className="active">Welcome</NavItem>
+            </LinkContainer>
+            <LinkContainer to={{ pathname: '/builder/questions' }}>
+              <NavItem>Questions</NavItem>
+            </LinkContainer>
+            <LinkContainer to={{ pathname: '/builder/review' }}>
+              <NavItem>Review</NavItem>
+            </LinkContainer>
+            <LinkContainer to={{ pathname: '/builder/products' }}>
+              <NavItem>Products</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar>
+      </Row>
+      <Grid>
+        <Row>
+          {props.children}
+        </Row>
+      </Grid>
+    </Grid>
   </div>
 );
 
--- imports/ui/customer/pages/WelcomePage.js
+++ imports/ui/customer/pages/WelcomePage.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 const WelcomePage = () => (
-  <div className="welcome-page">Welcome</div>
+  <div className="welcome-page">
+    <h1>Welcome!</h1>
+    <p>
+      Well, the way they make shows is, they make one show. That show's called
+      a pilot. Then they show that show to the people who make shows, and on
+      the strength of that one show they decide if they're going to make more
+      shows. Some pilots get picked and become television programs. Some
+      don't, become nothing. She starred in one of the ones that became
+      nothing.
+    </p>
+    <Button bsStyle="primary">
+      Get Started
+    </Button>
+  </div>
 );
 
 export default WelcomePage;
