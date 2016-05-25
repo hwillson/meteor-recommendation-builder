33c63ffa411855765b3e7299159e26c91b2c4402
hwillson <hugh@octonary.com>
2016-05-25 06:11:04 -0400

    Added comments to schema def for questions.

--- src/imports/api/questions/schema.js
+++ src/imports/api/questions/schema.js
@@ -1,15 +1,22 @@
 import { SimpleSchema } from '../../utility/meteor/packages';
 
 const questionSchema = new SimpleSchema({
+  // Question placeholder label displayed in answer box when no answer
+  // has been selected.
   label: {
     type: String,
   },
+  // Question content shown at the top of a question page within the wizard
+  // modal.
   question: {
     type: String,
   },
+  // Question summary content shown on the question overview page.
   summary: {
     type: String,
   },
+  // Question help content shown below the question content on a question page
+  // within the wizard modal.
   help: {
     type: String,
   },
