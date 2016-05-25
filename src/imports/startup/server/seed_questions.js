2d6cbbe9123d48ba07240f43add82a80d5ab7a8c
hwillson <hugh@octonary.com>
2016-05-25 05:56:06 -0400

    Made database seeding configurable.

--- src/imports/startup/server/seed_questions.js
+++ src/imports/startup/server/seed_questions.js
@@ -1,29 +1,31 @@
 import faker from 'faker';
-import { _ } from '../../utility/meteor/packages';
+import { Meteor, _ } from '../../utility/meteor/packages';
 
 import questions from '../../api/questions/collection';
 
-if (questions.find().count() === 0) {
-  _.times(5, () => {
-    const availableAnswers = [];
+if (Meteor.settings.private.seedDatabase) {
+  if (questions.find().count() === 0) {
     _.times(5, () => {
-      availableAnswers.push({
-        answerId: faker.random.number(),
-        answer: faker.lorem.words(),
-        imagePath: '/images/placeholder.png',
+      const availableAnswers = [];
+      _.times(5, () => {
+        availableAnswers.push({
+          answerId: faker.random.number(),
+          answer: faker.lorem.words(),
+          imagePath: '/images/placeholder.png',
+        });
+      });
+      questions.insert({
+        label: faker.lorem.word(),
+        question: faker.lorem.sentence(),
+        summary: faker.lorem.sentence(),
+        help: faker.lorem.sentence(),
+        order: faker.random.number(),
+        mandatory: true,
+        maxAnswersAllowed: 1,
+        matchKey: faker.lorem.word(),
+        matchExclusion: faker.random.boolean(),
+        availableAnswers,
       });
     });
-    questions.insert({
-      label: faker.lorem.word(),
-      question: faker.lorem.sentence(),
-      summary: faker.lorem.sentence(),
-      help: faker.lorem.sentence(),
-      order: faker.random.number(),
-      mandatory: true,
-      maxAnswersAllowed: 1,
-      matchKey: faker.lorem.word(),
-      matchExclusion: faker.random.boolean(),
-      availableAnswers,
-    });
-  });
+  }
 }
