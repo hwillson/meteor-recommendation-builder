edcb7ce280cc2bbbd8cc18448445223038ac185a
hwillson <hugh@octonary.com>
2016-06-10 13:55:19 -0400

    Wired up footer placeholder.

--- src/imports/ui/builder/components/footer/Footer.js
+++ src/imports/ui/builder/components/footer/Footer.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
 
 const Footer = () => (
-  <div className="footer">
-    TODO - footer ...
-  </div>
+  <footer className="footer">
+    <Row>
+      <Col mdOffset={1} md={2}>
+        <h3>Footer Menu 1</h3>
+        <ul>
+          <li><a href="#" target="_blank">Item 1</a></li>
+          <li><a href="#" target="_blank">Item 2</a></li>
+          <li><a href="#" target="_blank">Item 3</a></li>
+          <li><a href="#" target="_blank">Item 4</a></li>
+          <li><a href="#" target="_blank">Item 5</a></li>
+        </ul>
+      </Col>
+      <Col md={2}>
+        <h3>Footer Menu 2</h3>
+        <ul>
+          <li><a href="#" target="_blank">Item 1</a></li>
+          <li><a href="#" target="_blank">Item 2</a></li>
+          <li><a href="#" target="_blank">Item 3</a></li>
+          <li><a href="#" target="_blank">Item 4</a></li>
+          <li><a href="#" target="_blank">Item 5</a></li>
+        </ul>
+      </Col>
+      <Col md={2}>
+        <h3>Footer Menu 3</h3>
+        <ul>
+          <li><a href="#" target="_blank">Item 1</a></li>
+          <li><a href="#" target="_blank">Item 2</a></li>
+          <li><a href="#" target="_blank">Item 3</a></li>
+          <li><a href="#" target="_blank">Item 4</a></li>
+          <li><a href="#" target="_blank">Item 5</a></li>
+        </ul>
+      </Col>
+      <Col md={2}>
+        <h3>Footer Menu 4</h3>
+        <ul>
+          <li><a href="#" target="_blank">Item 1</a></li>
+          <li><a href="#" target="_blank">Item 2</a></li>
+          <li><a href="#" target="_blank">Item 3</a></li>
+          <li><a href="#" target="_blank">Item 4</a></li>
+          <li><a href="#" target="_blank">Item 5</a></li>
+        </ul>
+      </Col>
+      <Col md={2}>
+        <p className="copyright">Copyright Auto Awesome Inc.</p>
+        <ul>
+          <li><a href="#" target="_blank">Item 1</a></li>
+          <li><a href="#" target="_blank">Item 2</a></li>
+        </ul>
+      </Col>
+    </Row>
+  </footer>
 );
 
 export default Footer;
